9d76ccac241140b6f0f8ec6d3fba3b0426402274
aditya shah <adityashah@dhcp-rhodes-3405.redrover.cornell.edu>
2019-12-04 13:32:35 -0500

    Milestone 3

--- main (1).c
+++ main (1).c
@@ -121,7 +121,11 @@ ISR(PCINT0_vect){
 
 //Left connected 6(PD6,PCINT 22), Right connected to 7(PD7,PCINT23)
 ISR(PCINT2_vect){
-	if ((PIND & 0b01000000 == 0b010000000) && (PIND & 0b10000000 == 0b100000000)){ // if left and right detect black, then ???
+	printf("Comes to QTI Interrupt");
+	printf("\n");
+	printf("%d",PIND);
+	printf("\n");
+	if (((PIND & 0b01000000) == 0b01000000) && ((PIND & 0b10000000) == 0b10000000)){ // if left and right detect black, then ???
 			printf("Both detect black");
 			startright(); //turn 135 degrees
 			_delay_ms(900);
@@ -129,15 +133,16 @@ ISR(PCINT2_vect){
 		}
 
 		//else if left only, turn 45 to the right
-	else if (PIND & 0b01000000 == 0b010000000){
+	else if ((PIND & 0b01000000) == 0b01000000){
 			printf("Left detects black");
-			startright(); //turn 45 degrees
+			/*startright(); //turn 45 degrees
 			_delay_ms(300);
-			stopright();
+			stopright();*/
+			
 		}
 		
 		//else if right only, turn 45 to the left
-	else if (PIND & 0b10000000 == 0b100000000){
+	else if ((PIND & 0b10000000) == 0b10000000){
 			printf("Right detects black");
 			startleft(); //turn left 45 degrees
 			_delay_ms(300);
@@ -150,6 +155,9 @@ sei(); // Enable interrupts globally
 PCICR = 0b00000101; // Initialize Register B (color sensor) and D (QTI) for PC interrupts
 DDRB &= 0b11101111; // Pin PB4 is input pin
 DDRB |= 0b00100000; // Pin PB5 is output LED
+}
+
+void initimer(){
 TCCR1B |=0b00000001; // Set the timer prescaler to 1
 TCCR1B &=0b11111001;
 }
@@ -166,28 +174,22 @@ PCMSK0 |= 0b00010000; // Enable PB4 as a PC interrupt
 _delay_ms(10); // Give the interrupt a sec
 PCMSK0 &= 0b11101111; // Prevent further interrupts
 int time = period*.0625*2; // Change to units of microsecs
-printf("Period is %d",time);
-printf("\n");
+/*printf("Period is %d",time);
+printf("\n");*/
 if (time<200) return 0;
-else return 1
+else return 1;
 }
 
 
-/*int main(void){
-	while(1){
-	PORTB |= 0b00001001; //go forward
-
-	_delay_ms(1400);
-
-   	 PORTB &= 0b11110110;
-
-   	 _delay_ms(1400);
-	}
-}*/
-
 int main(void) {
 	init_uart(); // Initialize serial
-	initializeColor(); // Initialize the stuff
+	initializeColor(); // Initialize color sensor
+	initqti();
+	//For QTI Pin Change interupt, set appropriate pcvector, set app registers as output, use PCMSK to enable interrupts on those pins,
+	//initialize PCICR for app register
+	PCMSK2 |= 0b11000000; // Enable PD6,D7 as a PC interrupts
+	initimer();
+	
 	PORTB |= 0b00100000; // Set B5 as output
 	int initcolor = 0; // O if yellow, 1 if blue
 	//******************CHANGED*******************//
@@ -197,10 +199,7 @@ int main(void) {
 
 	DDRB = 0b00001111; 
 
-	//For QTI Pin Change interupt, set appropriate pcvector, set app registers as output, use PCMSK to enable interrupts on those pins,
-	//initialize PCICR for app register
-	PCMSK2 |= 0b11000000; // Enable PD6,D7 as a PC interrupts
-	initqti();
+
 
 	//******************CHANGED*******************//
 	//Timer 2 used to calculate time since color change
@@ -223,12 +222,12 @@ int main(void) {
 		}*/
 		//******************CHANGED*******************//
 
-		startforward()
+		startforward();
 		_delay_ms(75); //can vary
 
 		int currcolor = getColor();
-		printf("Color is: %i",currcolor);
-		printf("\n");
+		/*printf("Color is: %i",currcolor);
+		printf("\n");*/
 
 		//Turn around 180 degrees if you're not on your original color
 		if (currcolor != initcolor){
@@ -239,14 +238,16 @@ int main(void) {
 				TCNT2 = 0;
 			}*/
 			//******************CHANGED*******************//
- 
+			PCMSK2 &= 0b00000000; // Disable PD6,D7 as a PC interrupts
 			stopforward();
 			startright(); //turn 180 degrees
-			_delay_ms(1200);//adjust to make it 180 degreees
+			_delay_ms(1440);//adjust to make it 180 degreees
 			stopright();
-			startforward() //go ahead so you're back in the original color
+			startforward(); //go ahead so you're back in the original color
 			_delay_ms(400);
 			stopforward();
+			PCMSK2 |= 0b11000000; // Enable PD6,D7 as a PC interrupts
+
 		}
 
 	}
